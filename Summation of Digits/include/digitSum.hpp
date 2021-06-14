ffb90d86b77a26383204d4b2dfd7dcc10c37236d
S1N4M <sinamoghimi73@gmail.com>
2021-06-15 00:12:17 +0430

    digitSum revised

--- Summation of Digits/include/digitSum.hpp
+++ Summation of Digits/include/digitSum.hpp
@@ -10,7 +10,9 @@
 
 namespace math {
 
-int digitSum(const int &n) { return (n < 10) ? n : digitSum(n % 10 + n / 10); }
+// int digitSum(const int &n) { return (n < 10) ? n : digitSum(n % 10 + n / 10);
+// }
+int digitSum(const int &n) { return (n == 0) ? 0 : ((n % 9 == 0) ? 9 : n % 9); }
 
 } // namespace math
 
