7d0c6b009d045d1fb209f9454fda792694dece4b
S1N4M <sinamoghimi73@gmail.com>
2021-07-11 22:41:44 +0430

    Added notesCount to cpp file

--- All Combinations of Bank Notes Sum to N/bankNoteChanges.cpp
+++ All Combinations of Bank Notes Sum to N/bankNoteChanges.cpp
@@ -16,32 +16,33 @@ void LOG(const std::vector<int> &notes, std::vector<int> &coefficientVector,
 
 void printAllCombinations(int target, int current_sum, int start,
                           const std::vector<int> &notes,
-                          std::vector<int> &coeff) {
+                          std::vector<int> &coeff, const int &notesCount) {
 
   if (target == current_sum) {
-    LOG(notes, coeff, static_cast<int>(notes.size()));
+    LOG(notes, coeff, static_cast<int>(notesCount));
   }
 
-  for (int i{start}; i < notes.size(); ++i) {
+  for (int i{start}; i < notesCount; ++i) {
     int temp_sum{current_sum + notes.at(i)};
     if (temp_sum <= target) {
       coeff.at(i) += 1;
-      printAllCombinations(target, temp_sum, i, notes, coeff);
+      printAllCombinations(target, temp_sum, i, notes, coeff, notesCount);
       coeff.at(i) -= 1;
     } else
       return;
   }
 }
 
-void bankNotesCombination(const int &target, std::vector<int> &notes) {
-  std::vector<int> coeffVector(notes.size());
+void bankNotesCombination(const int &target, std::vector<int> &notes,
+                          const int &notesCount) {
+  std::vector<int> coeffVector(notesCount);
   std::sort(notes.begin(), notes.end());
-  printAllCombinations(target, 0, 0, notes, coeffVector);
+  printAllCombinations(target, 0, 0, notes, coeffVector, notesCount);
 }
 
 int main() {
   std::vector<int> notes{2, 1, 5};
   int noteSummation{};
   std::cin >> noteSummation;
-  bankNotesCombination(noteSummation, notes);
+  bankNotesCombination(noteSummation, notes, static_cast<int>(notes.size()));
 }
