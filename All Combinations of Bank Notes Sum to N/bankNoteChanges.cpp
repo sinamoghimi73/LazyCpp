ff4a9ac53d71ec86814aaf1e824cc4b08d005bf3
S1N4M <sinamoghimi73@gmail.com>
2021-07-13 00:55:20 +0430

    extracted functions from function printAllCombinations

--- All Combinations of Bank Notes Sum to N/bankNoteChanges.cpp
+++ All Combinations of Bank Notes Sum to N/bankNoteChanges.cpp
@@ -2,32 +2,46 @@
 #include <string>
 #include <vector>
 
-void LOG(const std::vector<int> &notes, std::vector<int> &coefficientVector,
-         const int &n) {
-  std::string output{};
+void buildOutput(std::string &output, const std::vector<int> &notes,
+                 const std::vector<int> &coefficientVector, const int &n) {
   for (unsigned int i{}; i < n; ++i)
     if (coefficientVector.at(i) != 0)
       output += (std::to_string(coefficientVector.at(i)) + "*" +
                  std::to_string(notes.at(i)) + " + ");
   output.pop_back();
   output.pop_back();
+}
+
+void print(const std::vector<int> &notes,
+           const std::vector<int> &coefficientVector, const int &n) {
+  std::string output{};
+  buildOutput(output, notes, coefficientVector, n);
   std::cout << output << "\n";
 }
 
-void generateAllCombinations(int target, int currentSum, int start,
-                             const std::vector<int> &notes,
-                             std::vector<int> &coeff, const int &notesCount) {
+void printAllCombinations(const std::vector<std::vector<int>> &combinations,
+                          const std::vector<int> &notes,
+                          const int &notesCount) {
+  for (const std::vector<int> &coefficients : combinations)
+    print(notes, coefficients, notesCount);
+}
 
-  if (target == currentSum) {
-    LOG(notes, coeff, static_cast<int>(notesCount));
+void generateAllCombinations(int target, int startIndex,
+                             const std::vector<int> &notes,
+                             std::vector<int> &coefficients,
+                             const int &notesCount,
+                             std::vector<std::vector<int>> &combinations) {
+
+  if (target == 0) {
+    combinations.push_back(coefficients);
   }
 
-  for (int i{start}; i < notesCount; ++i) {
-    if (currentSum + notes.at(i) <= target) {
-      coeff.at(i) += 1;
-      generateAllCombinations(target, currentSum + notes.at(i), i, notes, coeff,
-                              notesCount);
-      coeff.at(i) -= 1;
+  for (int i{startIndex}; i < notesCount; ++i) {
+    if (target - notes.at(i) >= 0) {
+      coefficients.at(i) += 1;
+      generateAllCombinations(target - notes.at(i), i, notes, coefficients,
+                              notesCount, combinations);
+      coefficients.at(i) -= 1;
     } else
       return;
   }
@@ -35,9 +49,13 @@ void generateAllCombinations(int target, int currentSum, int start,
 
 void bankNotesCombination(const int &target, std::vector<int> &notes,
                           const int &notesCount) {
-  std::vector<int> coeffVector(notesCount);
+  std::vector<std::vector<int>> combinations{};
+  std::vector<int> coefficientVector(notesCount);
   std::sort(notes.begin(), notes.end());
-  generateAllCombinations(target, 0, 0, notes, coeffVector, notesCount);
+  int startIndex{};
+  generateAllCombinations(target, startIndex, notes, coefficientVector,
+                          notesCount, combinations);
+  printAllCombinations(combinations, notes, notesCount);
 }
 
 int main() {
