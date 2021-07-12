b16ecaf5767748ad726b88c4b75fc26fd724a5fd
S1N4M <sinamoghimi73@gmail.com>
2021-07-13 01:02:46 +0430

    binaryGap created

---
+++ Binary Gap/binaryGap.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+class Solution {
+public:
+  int binaryGap(int n) {
+    int maxDistance{};
+    unsigned int counts{};
+
+    while ((n & 1) == 0) {
+      n >>= 1;
+    }
+
+    while (n > 0) {
+      if ((n & 1) == 1) {
+        if (counts > maxDistance)
+          maxDistance = counts;
+        counts = 0;
+      }
+      counts++;
+      n >>= 1;
+    }
+    return maxDistance;
+  }
+};
+
+int main() {
+  std::cout << Solution().binaryGap(22) << "\n";
+  return 0;
+}
